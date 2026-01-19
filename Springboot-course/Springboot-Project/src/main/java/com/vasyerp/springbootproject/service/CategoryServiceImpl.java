0648927fd0337470d29ed6a0f472e3cefb1023ce
Vivekchavda1374 <viveksinhchavda@gmail.com>
2026-01-19 12:39:32 +0530

    Spring boot project initialized and controller added

---
+++ Springboot-course/Springboot-Project/src/main/java/com/vasyerp/springbootproject/service/CategoryServiceImpl.java
@@ -0,0 +1,50 @@
+package com.vasyerp.springbootproject.service;
+
+import com.vasyerp.springbootproject.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+    private final List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
+    @Override
+    public List<Category> getAllCategories() {
+        return categories ;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
+        categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c->c.getCategoryId().equals(categoryId))
+                .findFirst()
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found"));
+        categories.remove(category);
+        return "Category with categoryId: " + categoryId + " deleted successfully !!";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> category1  = categories.stream()
+                .filter(c->c.getCategoryId().equals(categoryId))
+                .findFirst();
+        if(category1.isPresent()) {
+            Category existingCategory = category1.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+        }
+    }
+}
